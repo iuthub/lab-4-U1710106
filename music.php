e541a6aa2dff094845d753c85fdb564e438febb4
U1710106 <47245311+U1710106@users.noreply.github.com>
2019-02-21 19:24:22 +0500

    lab time ass

---
+++ music.php
@@ -0,0 +1,89 @@
+<<?php 
+	function convertBytes($bytes) 
+	{
+		if ($bytes < 1024) {
+			return $bytes." b";
+		}
+		else if ($bytes < 1024*1024) {
+			return round($bytes/1024, 2 )." kb";
+		}
+		else  {
+			return round($bytes/1024/1024, 2) ." mb";
+		}		
+	}
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+ "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Music Viewer</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<link href="viewer.css" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
+		<div id="header">
+
+			<h1>190M Music Playlist Viewer</h1>
+			<h2>Search Through Your Playlists and Music</h2>
+		</div>
+
+
+		<div id="listarea">
+			<ul id="musiclist">
+				<?php
+					if (isset($_REQUEST["playlist"])) {
+						$playlist=$_REQUEST["playlist"];
+						$file_contents=file_get_contents("songs/".$playlist);
+						$songs= explode("\n", $file_contents);
+						foreach ($songs as $song) {
+				?>		
+						<li class="mp3item">
+							<a href="<?="songs/*".$song ?>"><?=$song?></a>
+						</li>				
+				<?php		}
+					}
+					else
+					{
+						foreach (glob("songs/*.mp3") as $filename) 
+						{ ?>
+						<li class="mp3item">
+							<a href="<?= $filename?>"><?=basename($filename)?></a>
+							(<?=convertBytes(filesize($filename))?> b)
+						</li>
+						<?php
+						}
+						foreach (glob("songs/*.mp3") as $filename) { ?>
+						<li class="playlistitem">
+							<a href="music.php?playlist=<?=basename($filename)?>"><?=basename($filename)?></a>
+						</li>
+						<?php
+					}
+					}
+
+				?>
+
+				
+				
+				<li class="mp3item">
+					<a href="songs/Drift Away.mp3">Drift Away.mp3</a>
+					(5724612 b)
+				</li>
+
+				<li class="mp3item">
+					<a href="songs/Hello.mp3">Hello.mp3</a>
+
+					(1871110 b)
+				</li>
+
+				<li class="mp3item">
+					<a href="songs/Panda Sneeze.mp3">Panda Sneeze.mp3</a>
+					(58 b)
+				</li>
+				<li class="playlistitem">
+					<a href="music.php?playlist=playlist.txt">playlist.txt</a>
+				</li>
+			</ul>
+		</div>
+	</body>
+</html>
